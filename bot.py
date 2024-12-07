100b7be63afcecf6a7c9bf33f210d6270fd52308
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-12-07 08:04:09 -0300

    organizando

--- bot.py
+++ bot.py
@@ -33,32 +33,33 @@ async def on_guild_join(guild) -> None:
         await channel.send(f"Olá! Eu sou o Web Scraper e fui desenvolvido para realizar a tarefa de web scraping, para começar basta apenas digitar `!play` para receber mais informações no privado.")
     
 ''' Comando scraping '''
-reacted_messages = {}
+reacted_messages: dict = {}
 @bot.command()
 async def scraping(ctx, *, message: str) -> None:
-    if ctx.guild == None:
-        loading: str = await ctx.send("Coletando dados do produto... aguarde...")
+    if ctx.guild == None: # Se não for enviado no privado ele vai mostrar a mensagem
+        loading: str = await ctx.send("Coletando dados do produto... aguarde...") # Mensagem de loading
         try:
-            product: dict = await data_get(message)
+            product: dict = await data_get(message) # Coleta os dados do produto
             await ctx.send(f"**Produto:** {product['name'][0]}\n**Preço:** {product['price'][0]}" if len(product['name']) == 1 and len(product['price']) == 1 else f"**Produto:** {product['name']} \n**Preço:** {product['price']}") # Se tiver mais de um nome ou preço ele vai mostrar uma lista com os itens
 
-            is_correct = await ctx.send("Esse é o produto que você deseja monitorar?")
-            reacted_messages[is_correct.id] = {'processed': False, 'product': product} # marca essa mensagem como ainda não reagida
+            is_correct: str = await ctx.send("Esse é o produto que você deseja monitorar?")
+            reacted_messages[is_correct.id]: dict = {'processed': False, 'product': product} # marca essa mensagem como ainda não reagida
+
             await create_table(f"user_{ctx.author.id}") # Cria uma tabela com o id do usuario
-            await is_correct.add_reaction('✅') 
-            await is_correct.add_reaction('❌')
+            await is_correct.add_reaction('✅') # Adiciona duas reações
+            await is_correct.add_reaction('❌') 
 
         except:
-            await ctx.send("O link do produto é invalido.")
-        
+            await ctx.send("O link do produto é invalido.") # Se o link for invalido
+
 @bot.event
-async def on_reaction_add(reaction, user):
+async def on_reaction_add(reaction, user): 
     # Ignorar se for o próprio bot reagindo
     if user == bot.user:
         return
 
     if reaction.message.id in reacted_messages: # Verifica se o id da reação esta dentro das mensagens reagidas
-        reaction_info = reacted_messages[reaction.message.id] # Pega as informações da mensagem reagida pelo id
+        reaction_info: dict = reacted_messages[reaction.message.id] # Pega as informações da mensagem reagida pelo id
 
         if reaction_info['processed']:  # Se a chave 'processed' for True ele retorna nada
             return
@@ -72,7 +73,7 @@ async def on_reaction_add(reaction, user):
             elif reaction.emoji == '❌':
                 await reaction.message.channel.send(f'Entendi, para passar um novo produto é só digitar `!play` e passar o link na frente, não se esqueça.')
 
-            reacted_messages[reaction.message.id]['processed'] = True # Se qualquer reação for acionada, ela é marcada como True
+            reacted_messages[reaction.message.id]['processed']: bool = True # Se qualquer reação for acionada, ela é marcada como True
 
             # {"Id da mensagem": {'processed': True, 'product': {'name': ['Nome do produto'], 'price': ['preço do produto']}}}
 
--- database.py
+++ database.py
@@ -1,11 +1,13 @@
+''' Importações '''
 import sqlite3
 import asyncio
 
+''' Conecção e cursor com o DB '''
 conn = sqlite3.connect('database.db') # Estabelece conecção com o banco de dados
-
 cursor = conn.cursor() # Cria um cursor para interagir com a tabela
 
-async def create_table(user): # Função que vai criar uma tabela
+''' Funções para interação com o DB '''
+async def create_table(user: str): # Função que vai criar uma tabela
     cursor.execute(f'''
     CREATE TABLE IF NOT EXISTS {user} (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
