b00cec878f8822b358a78be2e5bbe675b44b5d13
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-12-08 10:08:14 -0300

    update scrapping
    retirei alguns e-comerces que não achei legal de colocar e fiz o scraping funcionar na Amazon, na Samsung e na AliExpress

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ from scrapping import data_get
 import asyncio
 
 async def main():
-    product = await data_get("https://shop.samsung.com/br/monitor-curvo-full-hd-samsung-led-27/p")
+    product = await data_get("https://www.amazon.com.br/Creme-Hidratante-453-G-Cetaphil/dp/B079VXJK79/ref=cm_gf_aabk_iaad_d_p0_e0_qd0_MYV46bXKoFvyyrTWJRX4?sbo=RZvfv%2F%2FHxDF%2BO5021pAnSA%3D%3D")
+
+    print(product)
+    product = await data_get("https://pt.aliexpress.com/item/1005007918633378.html?spm=a2g0o.tm1000009216.d5.1.2d016f3dRFGcEQ&sourceType=561&pvid=3448bf6d-c57c-4b16-9518-f83fc7c47d2c&pdp_ext_f=%7B%22ship_from%22:%22CN%22,%22sku_id%22:%2212000042844136157%22%7D&scm=1007.28480.379202.0&scm-url=1007.28480.379202.0&scm_id=1007.28480.379202.0&pdp_npi=4%40dis%21BRL%21R%24%20181%2C23%21R%24%2051%2C62%21%21%21209.47%2159.66%21%402103241117336621183758497e6c07%2112000042844136157%21gsd%21BR%21%21X&channel=sd&aecmd=true&_gl=1*8daruu*_gcl_au*MTQyNTYyODIzNC4xNzMzNjE4Nzgy*_ga*MTI4NDM0OTE4OS4xNzMzNjE4Nzgy*_ga_VED1YSGNC7*MTczMzY2MjA0MC4yLjEuMTczMzY2MjU4NS41Mi4wLjA.")
+
+    print(product)
+    product = await data_get("https://shop.samsung.com/br/galaxy-z-fold6/p")
 
     print(product)
 
--- scrapping.py
+++ scrapping.py
@@ -1,4 +1,5 @@
 from playwright.async_api import async_playwright
+
 import asyncio
 
 async def verify_commerce(link): # Função que vai verificar qual é o e-commerce
@@ -12,7 +13,6 @@ async def verify_commerce(link): # Função que vai verificar qual é o e-commer
         title = await page.title()
 
         await browser.close()
-        print(title)
 
         if "MercadoLivre" in title:
             return "MercadoLivre"
@@ -23,15 +23,99 @@ async def verify_commerce(link): # Função que vai verificar qual é o e-commer
         if "Amazon" in title:
             return "Amazon"
 
-        if "SHEIN" in title or "shein" in title or "Shein" in title:
-            return "Shein"
-
-        if "Attention Required!" in title:
-            return "OLX"
-        
         if "AliExpress" in title:
             return "AliExpress"
 
         return title
 
+async def data_get(link):
+    commerce = await verify_commerce(link)
+
+    if commerce == "Samsung":
+        return await is_samsung(link)
+
+    if commerce == "Amazon":
+        return await is_amazon(link)
+
+    if commerce == "AliExpress":
+        return await is_aliexpress(link)
+
+
+async def is_samsung(link):
+    async with async_playwright() as p: 
+        browser = await p.chromium.launch(headless=True)  # Abre uma pagina no chromium
+        page = await browser.new_page()  
+
+        await page.goto(link) 
+
+        await page.wait_for_selector(".samsungmaster-global-pdp-shop-4-x-productName")
+        await page.wait_for_selector(".samsungmaster-global-pdp-shop-4-x-sellingPrice")
+
+        name_quotes: list = await page.query_selector_all(".samsungmaster-global-pdp-shop-4-x-productName")
+        price_quotes: list = await page.query_selector_all(".samsungmaster-global-pdp-shop-4-x-sellingPrice")
+
+        name: list = await asyncio.gather(*(quote.inner_text() for quote in name_quotes)) # Espera a coleta do nome do produto
+        prices: list = await asyncio.gather(*(quote.inner_text() for quote in price_quotes)) # Espera a coleta do preço do produto
+
+        await browser.close()
+
+    return {
+        "name" : name,
+        "price" : [price[3:] for price in prices]
+    }
+
+async def is_amazon(link):
+    async with async_playwright() as p: 
+        browser = await p.chromium.launch(headless=True)  # Abre uma pagina no chromium
+        page = await browser.new_page()  
+
+        await page.goto(link) 
+
+        await page.wait_for_selector("#productTitle")
+        await page.wait_for_selector(".a-price-whole")
+
+        name_quotes: list = await page.query_selector_all("#productTitle")
+        price_quotes: list = await page.query_selector_all(".a-price-whole")
+
+        name: list = await asyncio.gather(*(quote.inner_text() for quote in name_quotes)) # Espera a coleta do nome do produto
+        prices: list = await asyncio.gather(*(quote.inner_text() for quote in price_quotes)) # Espera a coleta do preço do produto
+
+        await browser.close()
+
+    price = []
+    for _ in prices[0]:
+        if not _ in "1234567890":
+            break
+        else:
+            price.append(_)
+
+    return {
+        "name" : name,
+        "price" : ["".join(price)]
+    }
+
+async def is_aliexpress(link):
+    async with async_playwright() as p: 
+        browser = await p.chromium.launch(headless=True)  # Abre uma pagina no chromium
+        page = await browser.new_page()  
+
+        await page.goto(link) 
+
+        await page.locator('//*[@id="root"]/div/div[1]/div/div[1]/div[1]/div[2]/div[5]/h1').text_content()
+        await page.locator('//*[@id="root"]/div/div[1]/div/div[1]/div[1]/div[2]/div[2]/div[1]/span').text_content()
+
+        name_quotes: list = await page.locator('//*[@id="root"]/div/div[1]/div/div[1]/div[1]/div[2]/div[5]/h1').text_content()
+        price_quotes: list = await page.locator('//*[@id="root"]/div/div[1]/div/div[1]/div[1]/div[2]/div[2]/div[1]/span').text_content()
+
+
+        await browser.close()
+
+    return {
+        "name" : [name_quotes],
+        "price" : [price_quotes]
+    }
+
+
+
+# locator e query selector
 
